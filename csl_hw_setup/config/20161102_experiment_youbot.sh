d3e89c91e57cb355a370025dbe4d7e85fc0f1b55
Nikos Koukis <nickkouk@gmail.com>
2016-11-02 01:32:11 +0200

    Change launchfile docstring for setup_slam_agent.launch

--- csl_hw_setup/config/20161102_experiment_youbot.sh
+++ csl_hw_setup/config/20161102_experiment_youbot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Single robot SLAM 
+# source this file on both the ubuntu machine and youbot
 
 export MR_IS_MULTIROBOT_GRAPHSLAM=0
 export MR_IS_SIMULATION=0
@@ -9,7 +10,7 @@ export MR_ROBOT_ID=15
 export MR_ROBOT_MODEL="youbot"
 
 # Topics are to be recorded in the Ubuntu machine
-export MR_RECORD_TOPICS=0
+export MR_RECORD_TOPICS=1
 
 # Robot HW Drivers
 # Select only one of the following
@@ -24,7 +25,7 @@ export MR_LASER_NAME="hokuyo"
 export MR_LASER_PORT="/dev/ttyACM0"
 
 # Camera
-export MR_USE_CAMERA=0
+export MR_USE_ONBOARD_CAMERA=0
 export MR_CAMERA_PORT="dev/video0"
 
 # Teleoperation
@@ -34,4 +35,4 @@ export MR_USE_JOYSTICK=0
 export MR_ORIGIN_MARKER_ID="mf7"
 export MR_ROBOT_MARKER_ID="mf1"
 
-export MR_USE_ROOF_CAMERAS=1
+export MR_COMPUTE_GROUND_TRUTH=1
