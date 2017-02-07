effebabdc963b22105faf4b4b777f327cda5a752
Nikos Koukis <nickkouk@gmail.com>
2017-02-07 11:37:38 +0200

    WIP

--- csl_robots_gazebo/config/slam/simulation_config.sh
+++ csl_robots_gazebo/config/slam/simulation_config.sh
@@ -19,7 +19,7 @@ export MR_GLOBAL_FRAME_ID="map"
 # Open the rqt robot steering windows.
 export MR_USE_RQT_ROBOT_STEERING=1
 
-# Launch the graphSLAM-related nodes in differeent namespaces 
+# Launch the graphSLAM-related nodes in differeent namespaces
 export MR_USE_DIFFERENT_ROSCORES=1
 
 # TODO - fill in the necessary variables
@@ -41,7 +41,7 @@ export MR_ROBOT_1_MODEL="pioneer_3at"
 # define the robot name. This will also be the corresponding namespace
 export MR_ROBOT_1_NAME="${MR_HOSTNAME}_${MR_IP_LAST_FIELD}"
 
-export MR_ROBOT_1_POS_X=-1.49
+export MR_ROBOT_1_POS_X=-1.5
 export MR_ROBOT_1_POS_Y=-3
 export MR_ROBOT_1_POS_Z=0.051
 
@@ -51,14 +51,14 @@ export MR_ROBOT_1_ROT_Z=0
 
 
 # TODO - runtime error if I use CFixedIntervalsNRD here...
-export MR_ROBOT_1_NRD="CICPCriteriaNRD"
+export MR_ROBOT_1_NRD="CICPCriteriaNRD_CM"
 export MR_ROBOT_1_ERD="CLoopCloserERD_CM"
 export MR_ROBOT_1_GSO="CLevMarqGSO"
 
 # MR_ROBOT 2
 ######################
-export MR_ROBOT_2_MODEL="pioneer_3at"
-export MR_ROBOT_2_NAME="${MR_HOSTNAME}_11312_${MR_IP_LAST_FIELD}"
+#export MR_ROBOT_2_MODEL="pioneer_3at"
+#export MR_ROBOT_2_NAME="${MR_HOSTNAME}_11312_${MR_IP_LAST_FIELD}"
 
 export MR_ROBOT_2_POS_X=10
 export MR_ROBOT_2_POS_Y=-6
@@ -68,22 +68,26 @@ export MR_ROBOT_2_ROT_X=0
 export MR_ROBOT_2_ROT_Y=0
 export MR_ROBOT_2_ROT_Z=3.1415
 
-export MR_ROBOT_2_NRD="CICPCriteriaNRD"
+export MR_ROBOT_2_NRD="CICPCriteriaNRD_CM"
 export MR_ROBOT_2_ERD="CLoopCloserERD_CM"
 export MR_ROBOT_2_GSO="CLevMarqGSO"
 
 ###########################################3
 # TODO - Run simulation with 3 robots
 
-# TODO
 # define the robot name + corresponding namespace
 export MR_ROBOT_3_MODEL="pioneer_3at"
-#export MR_ROBOT_3_NAME="pioneer_3at_3"
+export MR_ROBOT_3_NAME="${MR_HOSTNAME}_11312_${MR_IP_LAST_FIELD}"
 
-export MR_ROBOT_3_POS_X=-1.49
-export MR_ROBOT_3_POS_Y=-6
+export MR_ROBOT_3_POS_X=-1.5
+export MR_ROBOT_3_POS_Y=-5.2
 export MR_ROBOT_3_POS_Z=0.051
 
 export MR_ROBOT_3_ROT_X=0
 export MR_ROBOT_3_ROT_Y=0
 export MR_ROBOT_3_ROT_Z=0
+
+export MR_ROBOT_3_NRD="CICPCriteriaNRD_CM"
+export MR_ROBOT_3_ERD="CLoopCloserERD_CM"
+export MR_ROBOT_3_GSO="CLevMarqGSO"
+
