e16fd7721c0e52165c5f27432d88cf4b6267a53c
Nikos Koukis <nickkouk@gmail.com>
2017-02-23 20:46:39 +0200

    Add new gazebo world + simul rosbags

--- csl_robots_gazebo/config/slam/simulation_config.sh
+++ csl_robots_gazebo/config/slam/simulation_config.sh
@@ -5,10 +5,10 @@ export MR_HOSTNAME="$(hostname)"
 # Last field of the IP of the computer that runs the simulation
 # e.g. 147.102.51.142 => 142
 # Used as a suffix in the name of the multimaster configuration
-export MR_IP_LAST_FIELD="$(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}' | cut -d "." -f4)"
+export MR_IP_LAST_FIELD="$(ifconfig wlan0 | awk '/inet addr/{print substr($2,6)}' | cut -d "." -f4)"
 
 # Setup one or two graphslam agents in the simulation
-export MR_IS_MULTIROBOT_GRAPHSLAM=0
+export MR_IS_MULTIROBOT_GRAPHSLAM=1
 
 # All nodes read this variable and output the messages accordingly
 export MR_OUTPUT_MESSAGES_TO="screen"
@@ -21,8 +21,6 @@ export MR_USE_RQT_ROBOT_STEERING=1
 
 # Launch the graphSLAM-related nodes in differeent namespaces
 export MR_USE_DIFFERENT_ROSCORES=1
-
-# TODO - fill in the necessary variables
 
 # Syntax for each robot properties
 # MR_ROBOT_X_PROPERTYNAME=PROPERTY_VALUE
@@ -41,17 +39,8 @@ export MR_ROBOT_1_MODEL="pioneer_3at"
 # define the robot name. This will also be the corresponding namespace
 export MR_ROBOT_1_NAME="${MR_HOSTNAME}_${MR_IP_LAST_FIELD}"
 
-export MR_ROBOT_1_POS_X=-1.5
-export MR_ROBOT_1_POS_Y=-3
-export MR_ROBOT_1_POS_Z=0.051
-
-export MR_ROBOT_1_ROT_X=0
-export MR_ROBOT_1_ROT_Y=0
-export MR_ROBOT_1_ROT_Z=0
-
-
-export MR_ROBOT_1_NRD="CICPCriteriaNRD"
-export MR_ROBOT_1_ERD="CLoopCloserERD"
+export MR_ROBOT_1_NRD="CICPCriteriaNRD_CM"
+export MR_ROBOT_1_ERD="CLoopCloserERD_CM"
 export MR_ROBOT_1_GSO="CLevMarqGSO"
 
 # MR_ROBOT 2
@@ -59,15 +48,8 @@ export MR_ROBOT_1_GSO="CLevMarqGSO"
 export MR_ROBOT_2_MODEL="pioneer_3at"
 export MR_ROBOT_2_NAME="${MR_HOSTNAME}_11312_${MR_IP_LAST_FIELD}"
 
-export MR_ROBOT_2_POS_X=10
-export MR_ROBOT_2_POS_Y=-6
-export MR_ROBOT_2_POS_Z=0.051
 
-export MR_ROBOT_2_ROT_X=0
-export MR_ROBOT_2_ROT_Y=0
-export MR_ROBOT_2_ROT_Z=3.1415
-
-export MR_ROBOT_1_NRD="CICPCriteriaNRD_CM"
+export MR_ROBOT_2_NRD="CICPCriteriaNRD_CM"
 #export MR_ROBOT_2_NRD="CFixedIntervalsNRD_CM"
 export MR_ROBOT_2_ERD="CLoopCloserERD_CM"
 export MR_ROBOT_2_GSO="CLevMarqGSO"
@@ -79,14 +61,15 @@ export MR_ROBOT_2_GSO="CLevMarqGSO"
 #export MR_ROBOT_3_MODEL="pioneer_3at"
 #export MR_ROBOT_3_NAME="${MR_HOSTNAME}_11312_${MR_IP_LAST_FIELD}"
 
-export MR_ROBOT_3_POS_X=-1.5
-export MR_ROBOT_3_POS_Y=-5.2
-export MR_ROBOT_3_POS_Z=0.051
-
-export MR_ROBOT_3_ROT_X=0
-export MR_ROBOT_3_ROT_Y=0
-export MR_ROBOT_3_ROT_Z=0
-
 export MR_ROBOT_3_NRD="CFixedIntervalsNRD_CM"
 export MR_ROBOT_3_ERD="CLoopCloserERD_CM"
 export MR_ROBOT_3_GSO="CLevMarqGSO"
+
+#export gazebo_world="simul"
+export gazebo_world="ktM"
+export num_robots=2 # how many to spawn
+
+
+# robot coordinates
+# http://stackoverflow.com/questions/6659689/referring-to-a-file-relative-to-executing-script
+source "${BASH_SOURCE%/*}/scenario_coords/${gazebo_world}.sh"
